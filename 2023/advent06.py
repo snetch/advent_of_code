00dc69108e3a24121a734c18d695173699ffef71
snetch <snetchj@gmail.com>
2023-12-08 01:35:15 -0500

    tweaked day 6

--- 2023/advent06.py
+++ 2023/advent06.py
@@ -1,11 +1,8 @@
-part = 2
+times = [ int(i) for i in input().split()[1:] ]
+distances = [ int(i) for i in input().split()[1:] ]
 
-if part == 1:
-    times = [ int(i) for i in input().split()[1:] ]
-    distances = [ int(i) for i in input().split()[1:] ]
-else:
-    times = [ int( "".join(  input().split()[1:]) ) ]
-    distances = [ int( "".join(  input().split()[1:]) ) ]
+times.append(int( "".join(  [str(i) for i in times]  ) ))
+distances.append(int( "".join(  [str(i) for i in distances]  ) ))
 
 ways_total = 1
 for race in range( len(times) ):
@@ -13,6 +10,7 @@ for race in range( len(times) ):
         if holding * (times[race]-holding) > distances[race]:
             ways = times[race] - 2*holding + 1
             ways_total *= ways
+            print(ways, ways_total)
             break
 
-print(ways_total)
+print("Part 1: second to last line, end. Part 2: last line, start")
